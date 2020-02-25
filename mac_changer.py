19b891ee753db6f27ce7ccc6f961e0f1bd9e3515
Jean Andrew Fuentes <jaf@Jeans-MacBook-Pro.local>
2020-02-25 21:07:37 +0800

    Refactor mac_changer

--- mac_changer.py
+++ mac_changer.py
@@ -3,24 +3,24 @@
 import subprocess
 import optparse
 
-subprocess.call("ifconfig")
+def change_mac(interface, mac_address):
+    print()
+    print("-"*60)
+    print("[+] Changing MAC address for {} to {}".format(interface, mac_address))
+
+    subprocess.call([ "ifconfig", interface, "down" ])
+    subprocess.call([ "ifconfig", interface, "hw", "ether", mac_address ])
+    subprocess.call([ "ifconfig", interface, "up" ])
 
 parser = optparse.OptionParser()
-
 parser.add_option('-i', '--interface', dest="interface" , help=' Interface to change its MAC address ') 
 parser.add_option('-m', '--mac', dest="mac_address" , help=' New MAC address to use') 
-
 (options, arguments) = parser.parse_args()
 
-interface=options.interface
-mac_address=options.mac_address
+# interface=options.interface
+# mac_address=options.mac_address
 
 # interface="wlan0"
 # mac_address="00.11.22.33.44.55"
 
-print("-"*60)
-print("[+] Changing MAC address for {} to {}".format(interface, mac_address))
-
-subprocess.call([ "ifconfig", interface, "down" ])
-subprocess.call([ "ifconfig", interface, "hw", "ether", mac_address ])
-subprocess.call([ "ifconfig", interface, "up" ])
+change_mac(options.interface, options.mac_address)
