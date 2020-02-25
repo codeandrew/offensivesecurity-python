142e0a8edcd37b951c5bc97b41bddb59925b5034
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-02-25 21:20:22 +0800

    Organize functions in mac_changer py

--- mac_changer.py
+++ mac_changer.py
@@ -3,6 +3,15 @@
 import subprocess
 import optparse
 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option('-i', '--interface', dest="interface",
+                      help=' Interface to change its MAC address ')
+    parser.add_option('-m', '--mac', dest="mac_address",
+                      help=' New MAC address to use')
+    return parser.parse_args()
+    
+
 def change_mac(interface, mac_address):
     print()
     print("-"*60)
@@ -12,15 +21,11 @@ def change_mac(interface, mac_address):
     subprocess.call([ "ifconfig", interface, "hw", "ether", mac_address ])
     subprocess.call([ "ifconfig", interface, "up" ])
 
-parser = optparse.OptionParser()
-parser.add_option('-i', '--interface', dest="interface" , help=' Interface to change its MAC address ') 
-parser.add_option('-m', '--mac', dest="mac_address" , help=' New MAC address to use') 
-(options, arguments) = parser.parse_args()
-
 # interface=options.interface
 # mac_address=options.mac_address
 
 # interface="wlan0"
 # mac_address="00.11.22.33.44.55"
 
+(options, arguments) = get_arguments()
 change_mac(options.interface, options.mac_address)
