bb53a272353fb2919f81db928f75b6f69ef3e62d
Jean Andrew Fuentes <jaf@Jeans-MacBook-Pro.local>
2020-02-25 20:56:20 +0800

    Handle argument mac_changer

--- mac_changer.py
+++ mac_changer.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface="wlan0"
-mac_address="00.11.22.33.44.55"
+subprocess.call("ifconfig")
 
-subprocess.call("ifconfig {} down".format(interface), shell=True)
-subprocess.call("ifconfig {} hw ether {}".format(interface, mac_address), shell=True)
-subprocess.call("ifconfig {} up".format(interface), shell=True)
+parser = optparse.OptionParser()
+
+parser.add_option('-i', '--interface', dest="interface" , help=' Interface to change its MAC address ') 
+parser.add_option('-m', '--mac', dest="mac_address" , help=' New MAC address to use') 
+
+(options, arguments) = parser.parse_args()
+
+interface=options.interface
+mac_address=options.mac_address
+
+# interface="wlan0"
+# mac_address="00.11.22.33.44.55"
+
+print("-"*60)
+print("[+] Changing MAC address for {} to {}".format(interface, mac_address))
+
+subprocess.call([ "ifconfig", interface, "down" ])
+subprocess.call([ "ifconfig", interface, "hw", "ether", mac_address ])
+subprocess.call([ "ifconfig", interface, "up" ])
