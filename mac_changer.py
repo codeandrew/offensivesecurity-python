0f16d432200f3617bcabaf81b8c5684785d04678
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-02-27 07:52:27 -0500

    Finalize Mac changer version 1

--- mac_changer.py
+++ mac_changer.py
@@ -15,40 +15,38 @@ def get_arguments():
         parser.error("[-] Please Specify an interface, use --help")
     if not options.mac_address:
         parser.error("[-] Please Specify an new MAC address, use --help")
-    
+    # For improvement add auto mac address 00:11:22:33:44:55
     return options
     
 def change_mac(interface, mac_address):
-    print()
+    print
     print("-"*60)
     print("[+] Changing MAC address for {} to {}".format(interface, mac_address))
 
     subprocess.call([ "ifconfig", interface, "down" ])
     subprocess.call([ "ifconfig", interface, "hw", "ether", mac_address ])
     subprocess.call([ "ifconfig", interface, "up" ])
+    print("-"*60)
 
 def get_current_mac(interface):
+    print
     ifconfig_result=subprocess.check_output(["ifconfig", interface])
-    print(ifconfig_result)
 
-    mac_adddress_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    mac_adddress_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
     if mac_adddress_search_result:
-        return mac_adddress_search_result(0)
+        return mac_adddress_search_result.group(0)
     else:
         print("[-] No MAC address")
 
-# interface=options.interface
-# mac_address=options.mac_address
-
-# interface="wlan0"
-# mac_address="00.11.22.33.44.55"
-
 options = get_arguments()
-print("Current MAC Adress: " + str(get_current_mac(options.interface)))
+current_mac = get_current_mac(options.interface)
+print("Current MAC Adress: {} in {}".format(str(current_mac), options.interface))
+
 change_mac(options.interface, options.mac_address)
 
 new_mac = get_current_mac(options.interface)
 if new_mac == options.mac_address:
+    print("[+] Interface: {}".format(options.interface))
     print("[+] MAC address Successfully changed to {}".format(new_mac))
 else:
     print("[-] MAC address did not get changed.")
