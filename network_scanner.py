0e5c21955b3ff060c35542dd17c0c199142076c6
jean_kali <jeanandrewfuentes@gmail.com>
2020-02-28 09:39:52 -0500

    Sending and Receiving Packets

--- network_scanner.py
+++ network_scanner.py
@@ -10,9 +10,13 @@ def scan(ip):
     arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     broadcast.show()
+    # 34. Combining Frames Review
 
     arp_request_broadcast = broadcast/arp_request
     print(arp_request_broadcast.summary())
     arp_request_broadcast.show()
+
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
 scan("10.0.2.1/24")
