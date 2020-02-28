d4fba29e5d3f50aee24c1095d10370b74b85eb39
jean_kali <jeanandrewfuentes@gmail.com>
2020-02-28 09:56:22 -0500

    Iterating over answered list and Analysing Packets

--- network_scanner.py
+++ network_scanner.py
@@ -16,7 +16,13 @@ def scan(ip):
     print(arp_request_broadcast.summary())
     arp_request_broadcast.show()
 
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    # print(answered_list.summary())
+
+    for e in answered_list:
+        # print(e[1].show())
+        print(e[1].psrc)
+        print(e[1].hwsrc)
+        print("-"*50)
 
 scan("10.0.2.1/24")
