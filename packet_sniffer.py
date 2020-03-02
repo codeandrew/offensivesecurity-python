3a33333764d19224a1b625083381af0adb1bb1f7
jean_kali <jeanandrewfuentes@gmail.com>
2020-03-02 11:56:50 -0500

    Packet Sniffer, sniff payload

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+from scapy.layers import http
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 def process_sniffed_packet(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            # print(packet)
+            print(packet.show())
+            print(packet[scapy.Raw].load)
 
 sniff("eth0")
