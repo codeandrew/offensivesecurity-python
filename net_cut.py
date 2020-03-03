56ff2e4fb46cfe3422ab13e91da680096100dfa8
jean_kali <jeanandrewfuentes@gmail.com>
2020-03-02 20:42:23 -0500

    Add restore and exception handling to net_cut

--- net_cut.py
+++ net_cut.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 import netfilterqueue
+import subprocess
 
 def proccess_packet(packet):
     print(packet)
     packet.accept()
 
-queue = netfilterqueue.NetfilterQueue()
-queue.bind(0, proccess_packet)
+subprocess.call("iptables -I FORWARD -j NFQUEUE --queue-num 0", shell=True)
 
-queue.run()
+try:
+    while True:
+        queue = netfilterqueue.NetfilterQueue()
+        queue.bind(0, proccess_packet)
+        queue.run()
+except KeyboardInterrupt:
+    subprocess.call("iptables --flush", shell=True)
+    print("\nStopped.. IP Tables Flushed")
