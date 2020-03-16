4c9781deec9baab9c0296ddf65d357059ea2263b
jean_kali <jeanandrewfuentes@gmail.com>
2020-03-16 05:42:14 -0400

    Create execute and report.py

---
+++ execute_and_report.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import subprocess, smtplib
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
