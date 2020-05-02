35e22107f63b6b3550c952d0782740cf385c36f7
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-05-02 16:25:47 +0800

    Organize to function

--- web-penetration/domain-crawler.py
+++ web-penetration/domain-crawler.py
@@ -2,8 +2,12 @@
 import requests
 
 url = "mail.google.com"
-try:
-    get_response = requests.get("http://{}".format(url))
-    print(get_response)
-except requests.exceptions.ConnectionError:
-    pass
+def request(url):
+    try:
+        get_response = requests.get("http://{}".format(url))
+        print(get_response)
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+request(url)
