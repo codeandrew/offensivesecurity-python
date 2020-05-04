97926e41f8e7408452f23ac40c41f667168e56c5
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-05-04 16:55:54 +0800

    Add updates in domain crawler

--- web-penetration/domain-crawler.py
+++ web-penetration/domain-crawler.py
@@ -1,15 +1,17 @@
 
 import requests
 
-url = "mail.google.com"
 def request(url):
     try:
         return requests.get("http://{}".format(url))
     except requests.exceptions.ConnectionError:
         pass
 
-domain_list = "subdomains.txt"
-target_url = "google.com"
+domain_list = "subdomains.txt" 
+#"https://raw.githubusercontent.com/codeandrew/SecLists/master/Miscellaneous/subdomain-list.txt"
+target_url = raw_input("Enter Target URL: \n")
+
+discovered_subdomain_list = []
 
 with open(domain_list, 'r') as wordlist_file:
     for line in wordlist_file:
@@ -17,3 +19,10 @@ with open(domain_list, 'r') as wordlist_file:
         response = request(full_url)
         if response:
             print("[+] Discovered subdomain --> {}".format(full_url))
+            discovered_subdomain_list.append(full_url)
+
+
+with open("{}-subdomains.txt".format(target_url), 'w') as f:
+    for item in discovered_subdomain_list:
+        print >> f, item
+        
