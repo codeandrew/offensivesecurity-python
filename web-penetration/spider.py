05a5717867ac8e28e4a0c4149855f71dc89d418d
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-05-04 18:59:30 +0800

    Finalize spider recursive crawling

--- web-penetration/spider.py
+++ web-penetration/spider.py
@@ -6,19 +6,22 @@ def extract_links_from(url):
     response = requests.get(url)
     return re.findall('(?:href=")(.*?)"', response.content)
 
+def crawl(url):
+    href_links = extract_links_from(url)
+    for link in href_links:
+        parsed_link = urlparse.urljoin(url, link)
+
+        if "#" in parsed_link:
+            parsed_link = parsed_link.split('#')[0]
+
+        if url in parsed_link and parsed_link not in target_links:
+            target_links.append(parsed_link)
+            print(parsed_link)
+            crawl(parsed_link)
+
+
 url = raw_input("Enter Target URL: \n")
 target_url = "https://{}".format(url)
-
 target_links = []
 
-href_links = extract_links_from(target_url)
-
-for link in href_links:
-    parsed_link = urlparse.urljoin(target_url, link)
-
-    if "#" in parsed_link:
-        parsed_link = parsed_link.split('#')[0]
-        
-    if parsed_link not in target_links:
-        target_links.append(parsed_link)
-        print(parsed_link)
+crawl(target_url)
