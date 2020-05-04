1e6bd0edbde5d31babb59397762a8888bf9c5e50
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-05-04 17:53:05 +0800

    Store links in list

--- web-penetration/spider.py
+++ web-penetration/spider.py
@@ -9,8 +9,11 @@ def extract_links_from(url):
 url = raw_input("Enter Target URL: \n")
 target_url = "https://{}".format(url)
 
+target_links = []
+
 href_links = extract_links_from(target_url)
 
 for link in href_links:
     parsed_link = urlparse.urljoin(target_url, link)
+    target_links.append(target_url)
     print(parsed_link)
