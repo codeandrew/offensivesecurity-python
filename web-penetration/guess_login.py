efac71a7ae170d53af3ea7b933debac275a2e6af
jean_kali <jeanandrewfuentes@gmail.com>
2020-05-07 04:48:12 -0400

    Extracting forms

--- web-penetration/guess_login.py
+++ web-penetration/guess_login.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
 import requests
+from BeautifulSoup import BeautifulSoup
 
 def request(url):
     try:
         return requests.get("http://{}".format(url))
     except requests.exceptions.ConnectionError:
         pass
+
+target_url="10.0.2.15/mutillidae/index.php?page=dns-lookup.php"
+response = request(target_url)
+
+parsed_html = BeautifulSoup(response.content)
+form_list = parsed_html.findAll("form")
+print(form_list)
