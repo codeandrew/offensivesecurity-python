0de0b16f8d6694a00d5264d799a18dadef9ae677
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-05-12 23:46:14 +0800

    Completed raw function

--- util/randomizer.py
+++ util/randomizer.py
@@ -1,9 +1,19 @@
 import string
 import random
 
-char = string.ascii_letters + string.digits
+characters = string.ascii_lowercase + string.digits
 
-def random(char):
+def randomize(char):
     return random.choice(list(char))
 
+mac_raw = []
 
+for i in range(6):
+    if len(mac_raw) is not 0:
+        mac_raw.append(":")
+    for ii in range(2):
+        mac_raw.append(randomize(characters))
+
+mac_address = "".join(mac_raw)
+
+print(mac_address)
