be83f739d9a885c51deea08f9e31976f9d24d5a6
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-08-24 01:29:00 +0800

    Update ssh_bruteforce.py

--- attacks/ssh_bruteforce.py
+++ attacks/ssh_bruteforce.py
@@ -26,7 +26,8 @@ def main():
     user = sys.argv[1]
     host = sys.argv[2]
 
-    with open("passwords.txt", "r") as f:
+    password_list="/usr/share/wordlists/rockyou.txt"
+    with open(password_list, "r", encoding='latin-1' ) as f:
         passwords = f.read().splitlines()
 
     for password in passwords:
